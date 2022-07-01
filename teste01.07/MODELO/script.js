fb04767e4d9c2f0779fd0b3030c67bde4d2dfd45
Guilherme Dutra <108353448+guilhermedutragarcia@users.noreply.github.com>
2022-07-01 11:51:23 -0300

    Update script.js

--- teste01.07/MODELO/script.js
+++ teste01.07/MODELO/script.js
@@ -2,12 +2,18 @@ function contar() {
     var inicio = document.getElementById('inicio')    
     var fim = document.getElementById('fim')
     var passo = document.getElementById('passo')
+    var res = document.getElementById('res')
 
-    if (Number(inicio.value.length) == 0 || Number(fim.value.lenth) == 0 || Number(passo.value.length) == 0)
-        window.alert('[ERRO] Verifique os números e insira novamente!')
-    else {
-        
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        alert('[ERRO] Verifique os números e insira novamente!')
+    }else {
+        res.innerHTML= "Contando"
+        var i = Number(inicio.value)
+        var f = Number(fim.value)
+        var p = Number (passo.value)
 
+        for (var c = i; c <= f; c += p)
+            res.innerHTML += ` ${c} `
     }
 
 
