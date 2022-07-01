55597a82a2f59252cb05e6286dd9136ae2b9e5b4
Guilherme Dutra <108353448+guilhermedutragarcia@users.noreply.github.com>
2022-07-01 11:05:44 -0300

    exercicio atualizado

--- teste01.07/MODELO/script.js
+++ teste01.07/MODELO/script.js
@@ -1,1 +1,18 @@
-.
+function contar() {
+    var inicio = document.getElementById('inicio')    
+    var fim = document.getElementById('fim')
+    var passo = document.getElementById('passo')
+
+    if (Number(inicio.value.length) == 0 || Number(fim.value.lenth) == 0 || Number(passo.value.length) == 0)
+        window.alert('[ERRO] Verifique os números e insira novamente!')
+    else {
+        
+
+    }
+
+
+
+
+
+
+}
