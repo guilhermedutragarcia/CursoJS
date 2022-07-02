7301d6601ca94bbaf3ae98ccf029958613412fd9
Guilherme Dutra <108353448+guilhermedutragarcia@users.noreply.github.com>
2022-07-02 19:36:53 -0300

    Update script.js
    Completo!

--- teste01.07/MODELO/script.js
+++ teste01.07/MODELO/script.js
@@ -3,22 +3,20 @@ function contar() {
     var fim = document.getElementById('fim')
     var passo = document.getElementById('passo')
     var res = document.getElementById('res')
-
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
         alert('[ERRO] Verifique os números e insira novamente!')
     }else {
-        res.innerHTML= "Contando"
+        res.innerHTML= "Contando: "
         var i = Number(inicio.value)
         var f = Number(fim.value)
         var p = Number (passo.value)
-
-        for (var c = i; c <= f; c += p)
+        if (i < f) {
+            for (let c = i; c <= f; c += p)
+            res.innerHTML += `${c} `
+        } else {
+            for (let c = i; c >= f; c -= p)
             res.innerHTML += ` ${c} `
-    }
-
-
-
-
-
-
+        }
+    res.innerHTML += "Parabéns!"  
+    } 
 }
