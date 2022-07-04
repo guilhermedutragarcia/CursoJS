650450a949167d04599a70ecb2fb0c2e97f2ebcd
Guilherme Dutra <108353448+guilhermedutragarcia@users.noreply.github.com>
2022-07-04 20:04:25 -0300

    #atualização

--- ex#07 array/script.js
+++ ex#07 array/script.js
@@ -1,10 +1,28 @@
 var num = document.getElementById('numero')
+var lista = document.getElementById('cxnum')
+var res = document.getElementById('res')
+var valores = []
+
+function Numero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else{
+        return false
+    }
+}
+function nalista(n, l) {
+    if(l.indexOf(Number(n)) != -1) {
+        return true
+    } else{
+        return false
+    }
+}
 
 function adicionar() {
-    if (Number(num.value.length == 0 || Number(num.value) > 100 )){
-        window.alert('[ERRO] Verifique o número!')
-    } else {
-        window.alert('Tudo ok!')
+    if(Numero(num.value) && !nalista(num.value, valores)){
+        window.alert('tudo ok')
+       
+    } else{
+         window.alert('[ERRO] Verifique o número!')
     }
-
 }
